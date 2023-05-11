3becd8ae427b6d400a3eb46dc2421787149ed071
Muhammad Zahid faiz <mrzahidfaiz@gmail.com>
2023-05-11 19:15:40 +0500

    add products on  HomePage

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import * as data from "../../data";
+import "./HomePage.css";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <main>
+      <section className="productList">
+        {data.products.map((product) => {
+          return (
+            <section className="products">
+              <div>
+                <img src={product.thumbnail} alt={product.title} />
+              </div>
+              <div>
+                <h2>{product.title}</h2>
+                <p>{product.description}</p>
+              </div>
+              <div>
+                <h3>Price: ${product.price}</h3>
+              </div>
+              <div className="cart_btn">
+                <button>Add to Cart</button>
+              </div>
+            </section>
+          );
+        })}
+      </section>
+    </main>
+  );
 };
 
 export default HomePage;
